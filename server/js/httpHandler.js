d9f58fda9024105daf79684409e5354120cc7c20
Adam Tiller <a.j.tiller@gmail.com>
2020-04-10 15:50:38 -0700

    Add swim instruction response to GET request.

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -15,6 +15,15 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
+  // if req.method === GET && req.url === '/'
+  if ( req.method === 'GET' && req.url === '/' ) {
+    // array of up down left right
+    let myResponses = ['up', 'down', 'left', 'right'];
+    // random int 0 through 3
+    let randNum = Math.floor(Math.random() * 4);
+    // make the data something or another (response.write array at index of random int)
+    res.write(myResponses[randNum]);
+  }
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
 };
